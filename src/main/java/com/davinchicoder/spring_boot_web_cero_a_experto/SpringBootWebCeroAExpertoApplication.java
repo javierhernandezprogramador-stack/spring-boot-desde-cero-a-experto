74d37a7fa71820c078e79ea961de55bc38577924
javierhernandezprogramador-stack <javier.hernandez.programador@gmail.com>
2026-01-10 15:16:18 -0600

    Inicio del proyecto

---
+++ src/main/java/com/davinchicoder/spring_boot_web_cero_a_experto/SpringBootWebCeroAExpertoApplication.java
@@ -0,0 +1,13 @@
+package com.davinchicoder.spring_boot_web_cero_a_experto;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringBootWebCeroAExpertoApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpringBootWebCeroAExpertoApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/davinchicoder/spring_boot_web_cero_a_experto/SpringBootWebCeroAExpertoApplicationTests.java
@@ -0,0 +1,13 @@
+package com.davinchicoder.spring_boot_web_cero_a_experto;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringBootWebCeroAExpertoApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
