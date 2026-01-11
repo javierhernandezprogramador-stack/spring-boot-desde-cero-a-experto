4abc39222ca79a9be483efa37bedd85f9c4fd294
javierhernandezprogramador-stack <javier.hernandez.programador@gmail.com>
2026-01-11 16:50:11 -0600

    Primer CRUD de spring terminado

--- src/main/java/com/davinchicoder/spring_boot_web_cero_a_experto/Product.java
+++ src/main/java/com/davinchicoder/spring_boot_web_cero_a_experto/Product.java
@@ -1,10 +1,15 @@
 package com.davinchicoder.spring_boot_web_cero_a_experto;
 
+import lombok.AllArgsConstructor;
 import lombok.Builder;
 import lombok.Data;
+import lombok.NoArgsConstructor;
 
 @Builder
 @Data
+@NoArgsConstructor
+@AllArgsConstructor
+
 public class Product {
     private Long id;
     private String name;
