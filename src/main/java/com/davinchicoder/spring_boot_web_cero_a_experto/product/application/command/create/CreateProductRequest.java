28646839813c7d943eb70d9d2c3edc336d61eec3
javierhernandezprogramador-stack <javier.hernandez.programador@gmail.com>
2026-01-15 10:50:12 -0600

    Modifique las anotaciones de CreateProductRequest
    Coloque (@Builder, @Getter) para que funcionara el mappeo entre ProductDto a CreateProductRequest.

--- src/main/java/com/davinchicoder/spring_boot_web_cero_a_experto/product/application/command/create/CreateProductRequest.java
+++ src/main/java/com/davinchicoder/spring_boot_web_cero_a_experto/product/application/command/create/CreateProductRequest.java
@@ -1,11 +1,11 @@
 package com.davinchicoder.spring_boot_web_cero_a_experto.product.application.command.create;
 
 import com.davinchicoder.spring_boot_web_cero_a_experto.common.mediator.Request;
+import lombok.Builder;
 import lombok.Getter;
-import lombok.RequiredArgsConstructor;
 
 @Getter
-@RequiredArgsConstructor
+@Builder
 
 //Es de tipo void ya que no devulve contenido la respuesta
 public class CreateProductRequest implements Request<Void> {
